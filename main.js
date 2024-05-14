de8ee078063ac6a1b58eb1f5594b6e324c1740c5
Igroman4ik <ni4000009@gmail.com>
2024-05-14 21:43:58 +0300

    modified:   css/style.min.css 	modified:   css/style.min.css.map 	new file:   img/checkmark.svg 	new file:   img/checkmark2.svg 	modified:   index.html 	modified:   main.js 	modified:   scss/style.scss

--- main.js
+++ main.js
@@ -5,25 +5,30 @@ form.addEventListener('submit', handleForm);
 function main() {
   var totalTime = document.getElementById("totalTime");
   var interval = document.getElementById("interval");
-  var btn = document.getElementById("btn");
+
 
   if (!totalTime.value == "" && !interval.value == "" && !(parseInt(totalTime.value) < 0) && !(parseInt(interval.value) < 0)) {
-    totalTime.setAttribute("class", "hide");
-    interval.setAttribute("class", "hide");
-    btn.setAttribute("class", "hide");
+    var colorArr = getColor();
+    console.log(colorArr);
+    if (colorArr.length == 0) {
+      alert("Укажите не менее одного цвета");
+      return;
+    }
+    form.setAttribute("class", "visually-hidden");
 
-    let color = getRandomColor()
+    var color = getRandomColor(colorArr)
+    console.log(color);
+
     document.body.style.backgroundColor = color;
+
     let timerId = setInterval(() => {
-      color = getRandomColor()
+      color = getRandomColor(colorArr)
       document.body.style.backgroundColor = color;
     }, interval.value * 1000)
 
     setTimeout(() => {
       clearInterval(timerId);
-      totalTime.setAttribute("class", "");
-      interval.setAttribute("class", "");
-      btn.setAttribute("class", "");
+      form.setAttribute("class", "");
 
       document.body.style.backgroundColor = "#e8e8e8";
     }, totalTime.value * 1000);
@@ -31,11 +36,19 @@ function main() {
   }
 }
 
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+function getColor() {
+  var checkbox = document.getElementsByClassName("checkbox");
+  console.log(checkbox);
+  var arr = new Array();
+  for (i = 0; i < checkbox.length; i++) {
+    if (checkbox[i].checked) {
+      arr.push(checkbox[i].id);
+    }
   }
-  return color;
+  return arr;
+}
+
+function getRandomColor(color) {
+  color.sort(() => Math.random() - 0.5);
+  return color[0];
 }
